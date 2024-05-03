eb6011b43a113b2a41f76e6b013c95622dab7d3c
ferym26 <ferym26@gmail.com>
2024-05-04 00:26:55 +0400

    werg

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on('connection', (ws) => {
-    console.log('Client connected');
+	console.log('Client connected');
 
-    ws.on('message', (message) => {
-        console.log('Received message:', message);
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
+	ws.on('message', (message, isBinary) => {
+		const text = isBinary ? message : message.toString();
+		console.log('Received message:', text);
+		wss.clients.forEach((client) => {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(text);
+			}
+		});
+	});
 });
